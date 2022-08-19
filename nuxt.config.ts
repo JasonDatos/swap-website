c92eceee6ad8f8dfcc48c41cfbfcc8424a57747c
YanDevDe <1526048+YanDevDe@users.noreply.github.com>
2022-08-19 22:39:54 +0200

    target: "static" not needed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    target: "static",
     css: [
         '@/assets/scss/main.scss',
     ],
